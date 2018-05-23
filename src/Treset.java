eda4830a36c357d43623c6f0dd78af33726628a4
alex.bozic21@gmail.com <Aq6xK3bWT9r7>
2018-05-23 17:45:14 +0200

    Added printRound & changeOrder methods

--- src/Treset.java
+++ src/Treset.java
@@ -14,13 +14,15 @@ public class Treset {
 
         ArrayList<Card> previousRound = new ArrayList<Card>();
 
+        int [] order ={0,1,2,3};
         for(int i=0;i<10;i++){
             ArrayList<Card> onTable = new ArrayList<Card>();
-            int [] order ={0,1,2,3};
-            for(int j=0;j<4;j++)
-                onTable.add(players.get(order[j]).nextCard(previousRound,onTable));//vsak vrze dol karto
 
-            printRound(order,onTable);
+            for(int j=0;j<4;j++) {
+                onTable.add(players.get(order[j]).nextCard(previousRound, onTable));//vsak vrze dol karto
+                printRound(order, onTable);
+            }
+            //printRound(order,onTable);
             order = changeOrder(order,onTable);//mogoce se dodat malo vec belezenja vsega nekam
             previousRound = onTable;
             onTable.clear();
@@ -33,12 +35,24 @@ public class Treset {
 
 
     public static int[] changeOrder(int [] oldOrder, ArrayList<Card> onTable){
-
-        return null;//@Alex correct order of next round
+        int lastPicked = Rules.pickUp(onTable);
+        int[] newOrder = new int[4];
+        for (int i = lastPicked; i < lastPicked + 4; i++ ) {
+            newOrder[i - lastPicked] = i % 4;
+        }
+        return newOrder;//@Alex correct order of next round
     }
 
     public static void printRound(int[] order, ArrayList<Card> onTable){
-
+        for (int i = 0; i<order.length; i++) {
+            System.out.print(order[i]);
+            System.out.print("\t");
+            try {
+                System.out.println(onTable.get(i));
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println();
+            }
+        }
         //print played cards and winner
     }
 
