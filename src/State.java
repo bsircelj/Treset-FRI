f221498aa0ccd20bdce0b3b078dc3b441e2ebd25
bsircelj <benosircelj@gmail.com>
2018-05-24 18:10:35 +0200

    modified state

--- src/State.java
+++ src/State.java
@@ -4,16 +4,25 @@ public class State {
     int playerToMove;
     ArrayList<ArrayList<Card>> playerHands;
     ArrayList<Card> discards;
-    Card.Color currentColor;
+    char currentColor;
+    int roundNo;
+    ArrayList<Card> onTable;
+    int[] roundWinner;
+    int [] order;
 
     public State(){
         playerToMove = 0;
-        discards = new ArrayList<Card>();
-        currentColor = null;
+        discards = new ArrayList<Card>();//karte ki so bile igrane prej in niso vec v igri
+        currentColor = 'N';//Brez barvanja -> miza je prazna
         playerHands = new ArrayList<ArrayList<Card>>();
         for(int i=0;i<4;i++){
            playerHands.add(new ArrayList<Card>());
         }
+        roundNo = 0;
+        onTable = new ArrayList<Card>();
+        roundWinner = new int[10];
+        int [] order2 = {0, 1, 2, 3};
+        order = order2;
 
     }
 
@@ -22,11 +31,47 @@ public class State {
         clone.playerToMove = this.playerToMove;
         for(int i=0;i<4;i++){
             clone.playerHands.set(i,new ArrayList(this.playerHands.get(i)));
+            clone.order[i] = this.order[i];
         }
         clone.discards = new ArrayList(discards);
-        //clone.currentColor = new Card.Color(currentColor);
+        clone.currentColor = this.currentColor;
+        clone.roundNo = this.roundNo;
+        clone.onTable = new ArrayList(this.onTable);
+        clone.roundWinner = this.roundWinner.clone();
 
         return clone;
+    }
+
+    public State cloneAndRandomize(){
+        State st = this.clone();
+        ArrayList<Card> deck = Rules.createDeck();
+        deck.removeAll(discards);
+        deck.removeAll(onTable);
+        deck.removeAll(playerHands.get(playerToMove));
+
+        int receiver=playerToMove;
+        for(int i=0;i<4;i++){
+            st.playerHands.get(i).clear();
+        }
+        for(int i=0;i<deck.size();i++){
+            receiver=++receiver%4;
+            if(receiver==playerToMove)
+                receiver++;
+            playerHands.get(receiver).add(deck.get(i));
+        }
+
+        return st;
+    }
+
+    public void doMove(Card playedCard){
+        playerHands.get(playerToMove).remove(playedCard);
+        onTable.add(playedCard);
+        if(onTable.size()>3){
+            int won = Rules.pickUp(this.onTable);
+
+        }else{
+            playerToMove=(playerToMove+1)%4;
+        }
     }
 
 }
