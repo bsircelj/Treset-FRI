b69eb333d8cc68cff07310aed458952da3385a3b
bsircelj <benosircelj@gmail.com>
2018-05-25 15:28:18 +0200

    Node modified.

--- src/Node.java
+++ src/Node.java
@@ -3,54 +3,41 @@ import java.util.TreeSet;
 
 public class Node {
 
-    int win,total;
+    int wins,visits,avails;
 
-    TreeSet<Node> children;
+    ArrayList<Node> childNodes;
     Node parent;
-    int player; //Kdo je na vrsti
-    int myId;//Id igralca, ki izvaja search. da vemo če izbiramo mi ali kdo drug
-    ArrayList<Card> onTable; //Karte igrane od ostalih to potezo
-    Card card; //Karto, ki jo igra kdor je na vrsti v tej potezi
-    boolean onColor;
-    ArrayList<Card> alreadyPlayed = new ArrayList<Card>();//Vse karte, ki so bile igrane od kogarkoli, bomo rabli za konstruiranje igre do konca
-    ArrayList<ArrayList<Card.Color>> missingColor = new ArrayList<>();
+    int playerJustMoved; //Kdo je na vrsti
+    Card move; //Karto, ki jo igra kdor je na vrsti v tej potezi
 
-    public Node(){
-
-    }
-    public Node(Node parent, int player,int myId, ArrayList<Card> alreadyPlayed, ArrayList<Card> onTable){
+    public Node(Card move, Node parent, int justMoved){
+        this.wins = 0;
+        this.visits = 0;
+        this.avails = 1;
+        this.playerJustMoved=justMoved;
+        childNodes = new ArrayList<Node>();
         this.parent=parent;
-        children  = new TreeSet<Node>();
-        win=total=0;
-        this.myId = myId;
-        this.player = player;
-        this.onTable = onTable;
-        this.alreadyPlayed=alreadyPlayed;
-        for(int i=0;i<4;i++){
-            missingColor.set(i,new ArrayList<Card.Color>());
-        }
-        onColor=false;
+        this.move=move;
     }
 
-    public void addCard(Card card){
-        this.card = card;
-        onColor=true;
+    public ArrayList<Card> getUntriedMoves(ArrayList<Card> legalMoves){
+        ArrayList<Card> untried = new ArrayList<Card>();
+        for(Node child:this.childNodes){
+            if(!legalMoves.contains(child.move)){
+                untried.add(child.move);
+            }
+        }
+
+        return untried;
+    }
+    public Node UCBSelectChild(ArrayList<Card> legalMoves){
+        return UCBSelectChild(legalMoves,0.7);
     }
 
-    public Node copy(){
-        Node copy = new Node(this.parent,this.player,this.myId,this.alreadyPlayed,this.onTable);
-        if(this.onColor){
-            copy.onColor=true;
-            copy.card=this.card;
-        }
-        return copy;
-    }
+    public Node UCBSelectChild(ArrayList<Card> legalMoves, double exploration){
 
-    public ArrayList<Card.Color> getMissing(int playerId){//metoda ki vrne seznam barv, za katere vemo, da jih igralec nima
-        if(playerId==myId){
-            throw new Error("Sami zase vemo kaj imamo v roki. Klican je bil napacen id.");
-        }
-        return missingColor.get(playerId);
+
+        return null;
     }
 
 
