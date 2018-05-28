1c0441ce9f7fac7e622c2507150b8c40d48d06c8
bsircelj <benosircelj@gmail.com>
2018-05-28 14:05:32 +0200

    Deleted Search. ISMCTS moved to MCPlayer.

--- src/MCPlayer.java
+++ src/MCPlayer.java
@@ -1,14 +1,75 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MCPlayer extends Player {
-    Search search;
 
     public MCPlayer(int id, ArrayList<Card> hand) {
         super(id, hand);
-        search = new Search();
     }
 
-    public Card nextCard(ArrayList<Card> previous,ArrayList<Card> table){
-        return search.nextCard(previous,table);
+    public Card nextCard(State state){
+        Card chosen = ISMCTS(state,1000);
+        hand.remove(chosen);
+        return chosen;
+    }
+
+    public Card ISMCTS(State rootstate, int itermax){
+        return ISMCTS(rootstate,itermax,false);
+    }
+
+    public Card ISMCTS(State rootstate, int itermax, boolean verbose){
+        Node rootnode = new Node();
+
+        for(int iter=0;iter<itermax;iter++){
+            Node node = rootnode;
+            //Random random = new Random();
+            Random random = new Random((long)1212993.500322);//Za testirat boljse, da vedno izbere isto vrednost, bomo ko vemo da dela na 'truly random' seed v zgornji vrstici
+
+            //Determinize
+            State state = rootstate.cloneAndRandomize();
+
+            //Select
+            while(!state.getMoves().isEmpty()&&node.getUntriedMoves(state.getMoves()).isEmpty()){//optimizacija: samo 1x se klice getMoves
+                node = node.UCBSelectChild(state.getMoves());
+                state.doMove(node.move);
+            }
+
+            //Expand
+            ArrayList<Card> untriedMoves = node.getUntriedMoves(state.getMoves());
+            if(!untriedMoves.isEmpty()){
+                Card m = untriedMoves.get((random.nextInt(untriedMoves.size())));
+                int player = state.playerToMove;
+                state.doMove(m);
+                node = node.addChild(m,player);
+            }
+
+            //Simulate
+            while(true){
+                ArrayList<Card> moves = state.getMoves();
+                if(moves.isEmpty())
+                    break;
+                state.doMove(moves.get(random.nextInt(moves.size())));//lahko se dodamo neko hevristiko
+            }
+
+            //Backpropagate
+            while(node!=null){
+                node.Update(state);
+                node = node.parent;
+            }
+
+        }
+        if(verbose){
+            System.out.print(rootnode.treeToString(0));
+        }else{
+            System.out.print(rootnode.chilrenToString());
+        }
+        Node result = rootnode.childNodes.get(0);
+        for(Node all:rootnode.childNodes){
+            if(all.visits>result.visits){
+                result=all;
+            }
+        }
+
+        return result.move;
     }
 }
